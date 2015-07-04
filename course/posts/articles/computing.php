507b9ca9f5fafdd053a8d2a0ef1e63bd3c01d732
Sammy Herring <sammy.herring@me.com>
2015-07-04 17:30:52 +0100

    v5.1
    Full web application support with transparent GUI and splash pages for
    all current iOS devices. Also includes new article pages for extended
    subjects.

---
+++ course/posts/articles/computing.php
@@ -0,0 +1,143 @@
+<?php
+	$Course_Title = 'Computing';
+	$Course_Banner = 'computing';
+	$Course_Icon = 'pic05';
+	$Course_Mantra = 'In our new Revision Updates core education package for A-Level we are currently supporting OCR B Salters from day one.';
+?>
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>RU iTunes U - <?php echo $Course_Title; ?></title>
+		<?php include('../../../includes/tracking_info.php'); ?>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<?php include('../../../includes/web_clip.php'); ?>
+		<script src='http://code.jquery.com/jquery-latest.min.js' type='text/javascript'></script>
+		<script>
+			$(document).ready(function(){
+			$('a[href^="#"]').on('click',function (e) {
+			e.preventDefault();
+
+			var target = this.hash;
+			var $target = $(target);
+
+			$('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    	}, 900, 'swing', function () {
+	        	window.location.hash = target;
+	    	});
+			});
+			});
+		</script>
+	</head>
+	<body>
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<header id="header">
+					<h1><a href="<?php echo $Course_Banner; ?>.php">RU iTunes U</a> by Revision Updates</h1>
+					<nav id="nav">
+						<ul>
+							<li><a href="#page-wrapper">Welcome</a></li>
+							<li>
+								<a class="icon fa-angle-down">Resources</a>
+								<ul>
+									<li><a href="http://revisionupdates.info/releases">RU Releases</a></li>
+									<li><a href="http://revisionupdates.info/ru-digi">RU Digi</a></li>
+									<li><a href="http://revisionupdates.info/ambassadors">Ambassadors</a></li>
+									<li><a href="http://revisionupdates.info/itunesu">iTunes U Download</a></li>
+									<li>
+										<a>Admin</a>
+										<ul>
+											<li><a href="http://revisionupdates.info/admin">Slack</a></li>
+											<li><a href="http://ru.sammyherring.co.uk">Calendar</a></li>
+										</ul>
+									</li>
+								</ul>
+							</li>
+							<li><a href="http://revisionupdates.info/course/posts" class="button">Go Back</a></li>
+						</ul>
+					</nav>
+				</header>
+
+			<!-- Main -->
+				<section id="main" class="container">
+					<header>
+						<h2><?php echo $Course_Title; ?></h2>
+						<p><?php echo $Course_Mantra; ?></p>
+					</header>
+					<div class="box">
+						<span class="image featured"><img src="http://revisionupdates.info/course/posts/images/<?php echo $Course_Banner; ?>_banner.jpg" alt="" /></span>
+						<header>
+						<h3>Announcements</h3>
+						<p>More content coming soon.</p>
+						</header>
+						<p>
+							<span class="image left"><img src="http://revisionupdates.info/course/posts/articles/images/icons/<?php echo $Course_Icon; ?>.png" alt="" /></span>Just to let you know, we will be publishing Revision Updates A-Level Edition officially as apart of our <strong>public release</strong> on the 27th August 2015 (although we will start adding people from the 20th). As apart of the release with v2.0 revision updates we will be offering a new standard of resources that are specifically designed for all of your A-Level needs. We have an ever-expanding database of resources that we will be making available from day one.
+						</p>
+
+					</div>
+				
+					<div class="row">
+						<div class="6u 12u(narrower)">
+
+							<section class="box special">
+								<span class="image featured"><img src="http://revisionupdates.info/course/posts/images/pic02.jpg" alt="" /></span>
+								<h3>RU Releases</h3>
+								<p>
+									Our largest learning resource with over 50 GB of data. With a student-centric focus, we cover multiple subjects aimed at both GCSE and A-Level (GCE) students.
+								</p>
+								<ul class="actions">
+									<li><a href="http://revisionupdates.info/releases" class="button alt">Learn More</a></li>
+								</ul>
+							</section>
+
+						</div>
+						<div class="6u 12u(narrower)">
+
+							<section class="box special">
+								<span class="image featured"><img src="http://revisionupdates.info/course/posts/images/pic03.jpg" alt="" /></span>
+								<h3>RU Digi</h3>
+								<p>
+									Yet another of Revision Updates innovative resources. A cloud based calendar system, where we track and monitor all revision sessions for your convenience.
+								</p>
+								<ul class="actions">
+									<li><a href="http://revisionupdates.info/ru-digi/" class="button alt">Learn More</a></li>
+								</ul>
+							</section>
+
+						</div>
+					</div>					</section>
+
+			<!-- Footer -->
+				<footer id="footer">
+					<ul class="icons">
+						<?php 
+							include('../../../includes/config/social_media_config.php');
+							$page_type = 'ru_digi';
+							include('../../../includes/social_media_formats/social_media_ru_digi.php');
+						?>
+					</ul>
+					<ul class="copyright">
+						<li>
+							<?php include('../../../includes/footer.php'); ?>
+						</li>
+					</ul>
+				</footer>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/jquery.scrollgress.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
--- includes/web_clip.php
+++ includes/web_clip.php
@@ -6,6 +6,9 @@
 	<link rel="icon" type="image/png" sizes="64x64" href="http://revisionupdates.info/images/favicons/favicon-64.png">
 	<link rel="icon" type="image/png" sizes="32x32" href="http://revisionupdates.info/images/favicons/favicon-32.png">
 	<link rel="icon" type="image/png" sizes="16x16" href="http://revisionupdates.info/images/favicons/favicon-16.png">
+	<meta name="msapplication-TileColor" content="#5AA1BD">
+	<meta name="msapplication-TileImage" content="http://revisionupdates.info/images/favicons/favicon-144.png">
+	<meta name="msapplication-config" content="http://revisionupdates.info/images/favicons/browserconfig.xml">
 	<link rel="apple-touch-icon" href="http://revisionupdates.info/images/favicons/favicon-57.png">
 	<link rel="apple-touch-icon" sizes="114x114" href="http://revisionupdates.info/images/favicons/favicon-114.png">
 	<link rel="apple-touch-icon" sizes="72x72" href="http://revisionupdates.info/images/favicons/favicon-72.png">
@@ -15,6 +18,71 @@
 	<link rel="apple-touch-icon" sizes="76x76" href="http://revisionupdates.info/images/favicons/favicon-76.png">
 	<link rel="apple-touch-icon" sizes="152x152" href="http://revisionupdates.info/images/favicons/favicon-152.png">
 	<link rel="apple-touch-icon" sizes="180x180" href="http://revisionupdates.info/images/favicons/favicon-180.png">
-	<meta name="msapplication-TileColor" content="#5AA1BD">
-	<meta name="msapplication-TileImage" content="http://revisionupdates.info/images/favicons/favicon-144.png">
-	<meta name="msapplication-config" content="http://revisionupdates.info/images/favicons/browserconfig.xml">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+	
+	    <!-- iPad retina portrait startup image -->
+    <link href="http://revisionupdates.info/images/favicons/startup-icons/1536x2008.png"
+          media="(device-width: 768px) and (device-height: 1024px)
+                 and (-webkit-device-pixel-ratio: 2)
+                 and (orientation: portrait)"
+          rel="apple-touch-startup-image">
+ 
+    <!-- iPad retina landscape startup image -->
+    <link href="http://revisionupdates.info/images/favicons/startup-icons/1496x2048.png"
+          media="(device-width: 768px) and (device-height: 1024px)
+                 and (-webkit-device-pixel-ratio: 2)
+                 and (orientation: landscape)"
+          rel="apple-touch-startup-image">
+ 
+    <!-- iPad non-retina portrait startup image -->
+    <link href="http://revisionupdates.info/images/favicons/startup-icons/768x1004.png"
+          media="(device-width: 768px) and (device-height: 1024px)
+                 and (-webkit-device-pixel-ratio: 1)
+                 and (orientation: portrait)"
+          rel="apple-touch-startup-image">
+ 
+    <!-- iPad non-retina landscape startup image -->
+    <link href="http://revisionupdates.info/images/favicons/startup-icons/748x1024.png"
+          media="(device-width: 768px) and (device-height: 1024px)
+                 and (-webkit-device-pixel-ratio: 1)
+                 and (orientation: landscape)"
+          rel="apple-touch-startup-image">
+ 
+    <!-- iPhone 6 Plus portrait startup image -->
+    <link href="http://revisionupdates.info/images/favicons/startup-icons/1242x2148.png"
+          media="(device-width: 414px) and (device-height: 736px)
+                 and (-webkit-device-pixel-ratio: 3)
+                 and (orientation: portrait)"
+          rel="apple-touch-startup-image">
+ 
+    <!-- iPhone 6 Plus landscape startup image -->
+    <link href="http://revisionupdates.info/images/favicons/startup-icons/1182x2208.png"
+          media="(device-width: 414px) and (device-height: 736px)
+                 and (-webkit-device-pixel-ratio: 3)
+                 and (orientation: landscape)"
+          rel="apple-touch-startup-image">
+ 
+    <!-- iPhone 6 startup image -->
+    <link href="http://revisionupdates.info/images/favicons/startup-icons/750x1294.png"
+          media="(device-width: 375px) and (device-height: 667px)
+                 and (-webkit-device-pixel-ratio: 2)"
+          rel="apple-touch-startup-image">
+ 
+    <!-- iPhone 5 startup image -->
+    <link href="http://revisionupdates.info/images/favicons/startup-icons/640x1096.png"
+          media="(device-width: 320px) and (device-height: 568px)
+                 and (-webkit-device-pixel-ratio: 2)"
+          rel="apple-touch-startup-image">
+ 
+    <!-- iPhone < 5 retina startup image -->
+    <link href="http://revisionupdates.info/images/favicons/startup-icons/640x920.png"
+          media="(device-width: 320px) and (device-height: 480px)
+                 and (-webkit-device-pixel-ratio: 2)"
+          rel="apple-touch-startup-image">
+ 
+    <!-- iPhone < 5 non-retina startup image -->
+    <link href="http://revisionupdates.info/images/favicons/startup-icons/320x460.png"
+          media="(device-width: 320px) and (device-height: 480px)
+                 and (-webkit-device-pixel-ratio: 1)"
+          rel="apple-touch-startup-image">
